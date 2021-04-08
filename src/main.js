a28e0ca43e52ecd8b87be41a25e8476ffc779141
zhang0319 <8765417+zhang03199@user.noreply.gitee.com>
2021-04-08 15:43:08 +0800

    初始化项目目录2

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 // 加载全局样式
-import './styles/index.less'
-Vue.config.productionTip = false
+import "./styles/index.less";
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = []
+const routes = [];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
